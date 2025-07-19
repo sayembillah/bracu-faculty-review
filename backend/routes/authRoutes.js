6bd0d700b464feae75416aef2b90896dfcd093e1
sayembillah <sayembillah2000@gmail.com>
2025-07-19 14:36:15 +0600

    Setting up back end, added user login api

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // Auth Routes
 router.post("/register", registerUser); //Register a new user or admin
+router.post("/login", loginUser); //Login user or admin
 
 export default router;
